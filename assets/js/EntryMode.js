59213f4cec2e439fe883cec51005adfc80f4ddbc
Sybren Dijkstra <nerbiz@gmail.com>
2020-07-02 13:27:24 +0200

    Improved entry mode handling

--- assets/js/EntryMode.js
+++ assets/js/EntryMode.js
@@ -1,24 +1,53 @@
 /**
  * Several entry mode types, to be used as constants
- * @type {string}
+ * @type {number}
  */
-EntryMode.MODE_VALUE = 'value';
-EntryMode.MODE_CORNER = 'corner';
-EntryMode.MODE_CENTER = 'center';
+EntryMode.MODE_VALUE = 1;
+EntryMode.MODE_CORNER = 2;
+EntryMode.MODE_CENTER = 3;
 
 export default function EntryMode() {
     const self = this;
 
     /**
      * The current mode
-     * @type {string}
+     * @type {number}
+     * @private
      */
-    self.mode = EntryMode.MODE_VALUE;
+    let _mode = self.MODE_VALUE;
 
     /**
-     * Change the mode
-     * @param {string} mode
-     * @return {string}
+     * @param {number} mode
+     * @return {void}
      */
-    self.setMode = mode => self.mode = mode;
+    self.setMode = mode => {
+        if ((typeof mode).toLowerCase() !== 'number') {
+            throw new Error(`Expected a number, got ${typeof mode}`);
+        }
+
+        if (mode < self.MODE_VALUE || mode > self.MODE_CENTER) {
+            throw new Error('Invalid entry mode number given, please use EntryMode constants');
+        }
+
+        _mode = mode;
+    };
+
+    /**
+     * Change the mode number incrementally
+     * @return {void}
+     */
+    self.changeMode = () => {
+        // Increase the mode number
+        _mode++;
+
+        // Wrap around, when max number is reached
+        if (_mode > self.MODE_CENTER) {
+            _mode = self.MODE_VALUE;
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    self.getMode = () => _mode;
 }
