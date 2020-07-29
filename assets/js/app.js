225fe0c87b3968e1d58f99159d79785983b8be17
Sybren Dijkstra <nerbiz@gmail.com>
2020-07-29 17:00:24 +0200

    Added more test cases for state compression

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,20 +35,63 @@ Sudoku.documentEventHandler.register();
 
 /*
 const LZString = require('lz-string');
-const state = JSON.stringify(Sudoku.grid.getState());
-// 3 digits per cell: 2 digits for cell number, 1 for value
-// const state = '038149156382416811295104457093378472567629123781806';
-// 81 numbers, representing every cell value (0 = empty)
-// const state = '000300004000000056780000001230000045000098700012000045000005600300002300001320004';
-const c1 = LZString.compress(state);
-const c2 = LZString.compressToEncodedURIComponent(state);
 
-console.log(
-    state.length,
-    state,
-    c1.length,
-    c1,
-    c2.length,
-    c2
-);
+function logResults(state, label = null) {
+    const result1 = LZString.compress(state);
+    const result2 = LZString.compressToEncodedURIComponent(state);
+
+    console.group(label || 'Compression result');
+    console.log('Original state', state.length, state);
+    console.log('Compressed state', result1.length, result1);
+    console.log('URL-encoded compressed state', result2.length, result2);
+    console.groupEnd();
+}
+
+// The state from the application
+// logResults(JSON.stringify(Sudoku.grid.getState()), 'App');
+
+// 3 digits per cell: 2 digits for cell number, 1 for value
+const threeDigits = '038149156382416811295104457093378472567629123781806';
+logResults(threeDigits, '3 digits per cell');
+
+// 81 numbers, representing every cell value (0 = empty)
+const eightyOneNumbers = '000300004000000056780000001230000045000098700012000045000005600300002300001320004';
+logResults(eightyOneNumbers, '81 numbers');
+
+// A 1 as a counter, so the the digit 2 is '11', and 7 is '1111111' (delimited by a comma)
+const csvOnes = eightyOneNumbers.split('')
+    .map(number => {
+        number = parseInt(number, 10);
+        return '1'.repeat(number);
+    })
+    .join(',');
+logResults(csvOnes, 'CSV, counting with ones');
+
+// Grid cells state
+const cellsState = ('n' + '1'.repeat(23) // Cell number
+    + 'p' // Is prefilled, empty string when not prefilled
+    + 'v' + '1'.repeat(7) // Cell value
+    + 'c' + '1'.repeat(4) // Color number 4
+    + 'cr' + [3, 4, 6, 8].map(digit => {
+        digit = parseInt(digit, 10);
+        return '1'.repeat(digit);
+    }).join(',') // Corner pencil marks
+    + 'cn' + [1, 2, 7, 9].map(digit => {
+        digit = parseInt(digit, 10);
+        return '1'.repeat(digit);
+    }).join(',') // Center pencil marks
+).repeat(81) // For 81 cells
+// 366 length compressed, 975 length URL-encoded compressed
+logResults(cellsState, 'Cells state v1');
+
+// Grid cells state, normal way
+const cellsState2 = ('n23' // Cell number
+    + 'p' // Is prefilled, empty string when not prefilled
+    + 'v7' // Cell value
+    + 'c4' // Color number 4
+    + 'cr' + [3, 4, 6, 8].join('') // Corner pencil marks
+    + 'cn' + [1, 2, 7, 9].join('') // Center pencil marks
+).repeat(81) // For 81 cells
+// 118 length compressed, 315 length URL-encoded compressed
+logResults(cellsState2, 'Cells state v2');
 //*/
