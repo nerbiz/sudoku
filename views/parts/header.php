38c0933220f0da8dd19ad16acc12cd669a0cc9d1
Sybren Dijkstra <nerbiz@gmail.com>
2020-08-23 13:16:49 +0200

    Added icons to the top-bar buttons

--- views/parts/header.php
+++ views/parts/header.php
@@ -1,10 +1,9 @@
 <header id="top-bar">
     <div id="clock-wrapper">
-        <a href="#" id="toggle-pause-button" class="click-prevent">
+        <a href="#" id="toggle-pause-button" class="link-icon click-prevent">
             <i id="game-pause-icon" class="fas fa-pause-circle"></i>
             <i id="game-resume-icon" class="hide fas fa-play-circle"></i>
-        </a>
-        <span id="elapsed-time">0:00</span>
+        </a><span id="elapsed-time">0:00</span>
     </div>
 
     <h1 class="page-title" id="top-bar-title">
@@ -14,21 +13,24 @@
     <nav id="top-bar-menu">
         <ul>
             <li>
-                <a href="#" id="check-errors" class="click-prevent">Check for errors</a>
+                <a href="#"
+                   id="check-errors"
+                   class="click-prevent"
+                ><i class="link-icon fas fa-check"></i>Check for errors</a>
             </li>
 
             <li>
                 <a href="#"
                    class="click-prevent open-modal"
                    data-modal-id="settings-modal"
-                >Settings</a>
+                ><i class="link-icon fas fa-cog"></i>Settings</a>
             </li>
 
             <li>
                 <a href="#"
                    class="click-prevent open-modal"
                    data-modal-id="controls-modal"
-                >Controls</a>
+                ><i class="link-icon fas fa-info-circle"></i>Controls</a>
             </li>
         </ul>
     </nav>
